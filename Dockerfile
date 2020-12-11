83cce28a75d41b98547f25385643787fe77449ba
hamdardSingh <hamdard.kang@gmail.com>
2020-12-12 00:12:27 +0100

    minor change

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM alpine:3.12
 COPY entrypoint.sh /entrypoint.sh
+RUN ["chmod", "+x", "/entrypoint.sh"]
 RUN apk update
 RUN apk add openssh
 RUN apk add lftp
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
 echo "welcome to sftp deploy!"
-lftp -c "set sftp:auto-confirm yes ; open -u \"$INPUT_USER,$INPUT_PASSWORD\" \"$INPUT_HOST\" ; mirror -c -L -R -p -vvv $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
+lftp -c "set sftp:auto-confirm yes ; open -u \"$INPUT_USER,$INPUT_PASSWORD\" \"$INPUT_HOST\" ; mirror --only-newer -L -R -p -vvv $INPUT_LOCALDIR $INPUT_REMOTEDIR; quit"
